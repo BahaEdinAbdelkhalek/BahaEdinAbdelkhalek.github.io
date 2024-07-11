f78bc43ca4fbb96427ac534a47f119c8eedce960
Baha Edin Abdelkahlek <61942790+BahaEdinAbdelkhalek@users.noreply.github.com>
2024-07-11 20:47:57 +0100

    Update article.js

--- js/article.js
+++ js/article.js
@@ -16,3 +16,75 @@ function handleScroll() {
 }
 
 window.addEventListener('scroll', handleScroll);
+
+
+
+
+
+var elementToChange = document.getElementsByTagName("body")[0];
+elementToChange.style.cursor = "url('../assets/cursor.png'), auto";
+
+ 
+// Function to change cursor
+function changeCursor(element, cursorUrl) {
+  element.style.cursor = `url('${cursorUrl}'), auto`;
+}
+
+// Function to reset cursor
+function resetCursor(element) {
+  element.style.cursor = 'default';
+}
+
+
+
+// Get all elements you want to apply this effect to
+// For example, let's apply it to all div elements
+const elements = document.getElementsByTagName('a');
+
+// Loop through each element and add event listeners
+for (let element of elements) {
+  element.addEventListener('mouseover', function() {
+    changeCursor(this, '../assets/pointer.png');
+  });
+
+
+  element.addEventListener('mouseout', function() {
+    resetCursor(this);
+  });
+}
+
+
+
+// Get all elements you want to apply this effect to
+// For example, let's apply it to all div elements
+const elementss = document.getElementsByTagName('input');
+
+// Loop through each element and add event listeners
+for (let element of elementss) {
+  element.addEventListener('mouseover', function() {
+    changeCursor(this, '../assets/pointer.png');
+  });
+
+
+  element.addEventListener('mouseout', function() {
+    resetCursor(this);
+  });
+}
+
+
+
+// Get all elements you want to apply this effect to
+// For example, let's apply it to all div elements
+const elementsss = document.getElementsByTagName('button');
+
+// Loop through each element and add event listeners
+for (let element of elementsss) {
+  element.addEventListener('mouseover', function() {
+    changeCursor(this, '../assets/pointer.png');
+  });
+
+
+  element.addEventListener('mouseout', function() {
+    resetCursor(this);
+  });
+}
