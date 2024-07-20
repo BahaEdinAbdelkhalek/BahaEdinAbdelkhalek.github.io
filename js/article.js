5b3177b5dc96e958fd7c7209ddf0262cb83ad87d
bahaedin24 <bahabahabaha2482003@gmail.com>
2024-07-20 22:34:02 +0100

    other changes

--- js/article.js
+++ js/article.js
@@ -15,7 +15,7 @@ document.addEventListener("DOMContentLoaded", function() {
   });
   
 });
-
+/*
 
 const shrinkingContainer = document.getElementById('shrinkingContainer');
 const initialHeight = shrinkingContainer.offsetHeight;
@@ -41,7 +41,7 @@ window.addEventListener('scroll', handleScroll);
 
 
 
-
+*/
 
 var elementToChange = document.getElementsByTagName("body")[0];
 elementToChange.style.cursor = "url('../assets/cursor.png'), auto";
